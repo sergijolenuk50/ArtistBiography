e2db3cbce4edcf9d1043af92e67ed7c54a9a6cc9
sergijolenuk50 <sergijolenuk50@gmail.com>
2024-07-10 00:18:04 +0300

    create acthor Repin

---
+++ Controllers/BiographyController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class BiographyController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ReligiousCompositions()
+        {
+            return View();
+        }
+        public IActionResult Portrait()
+        {
+            return View();
+        }
+
+       
+        public IActionResult SelectedWorks()
+        {
+            return View();
+        }
+    }
+}
